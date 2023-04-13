ba3d9e79911462707d28d514342c4e8bd8ee628f
Paweł Stachaczyński <pawel.stachaczynski@gmail.com>
2023-04-14 00:10:18 +0200

    test2

--- YourPetFrontend/src/app/app.module.ts
+++ YourPetFrontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserPanelComponent } from './user-panel/user-panel.component';
 import { UserNavigationComponent } from './user-panel/user-navigation/user-navigation.component';
-// test
+// test1
 @NgModule({
   declarations: [
     AppComponent,
