ae2dbd92704533f183db97adeb85ae523c42f159
Paweł Stachaczyński <pawel.stachaczynski@gmail.com>
2023-04-18 17:43:20 +0200

    merge

---
+++ YourPetFrontend/src/app/user-panel/user-panel-main-page/user-panel-main-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-panel-main-page',
+  templateUrl: './user-panel-main-page.component.html',
+  styles: [
+  ]
+})
+export class UserPanelMainPageComponent {
+
+}
